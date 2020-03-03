fb8970826a81453dd2a0c5c82ac1d4c5daa668ab
Przemyslaw Rudzki <przemek.rudzki@gmail.com>
2020-03-02 20:26:30 -0500

    update to server.js

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const fs = require(`fs`);
+
 const express = require('express');
+
 const app = express();
-const PORT = process.env.PORT || 3000;
+
+const PORT = process.env.PORT || 5000;
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+
+
 
 app.get(`/notes`,(req,res)=>{
     let output = fs.readFileSync('./public/notes.html');
@@ -29,6 +35,7 @@ app.get('/',(req,res)=>{
     let output = fs.readFileSync('./public/index.html');
     res.end(output);
 });
+
 
 app.get('*',function (req, res) {
     res.redirect('/');
